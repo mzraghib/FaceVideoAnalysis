4f80236f6a599a099464b3b7fd0db25865661666
Muhammad Zuhayr Raghib <mzraghib@bu.edu>
2018-02-26 11:12:42 -0500

    updated stage 2 files

--- stage 2/random_forest.py
+++ stage 2/random_forest.py
@@ -1,108 +1,91 @@
 import numpy as np
 import os
 import pandas as pd
-
-
-def features(feat_dir, label_folder):
-    
-    labels = pd.read_csv(feat_dir)
-    
-    
-    
-    # read all .txt files (video file features)
-    all_files = os.listdir(label_folder)
-    
-    # 'sorted' doesn't work...not needed currently
-    """ build X and y """
-    X = np.zeros(shape=(len(all_files),431)) # change 431 to only number of required columns
-    y = np.zeros(len(all_files))
-    X_entry = np.zeros(431)
-    
-    i = 0
-    for data_file in sorted(all_files):
-        
-        print("processing    " + data_file)
-        
-        # X    
-        #TODO: Get rid of unneccessary columns from X
-    
-        Features = pd.read_csv(label_folder + data_file)
-        
-        j = 0
-        for column in Features:
-    #        X_entry = np.append(X_entry, Features[column][:].mean(axis=0))
-            X_entry[j] =  Features[column][:].mean(axis=0)
-            j += 1
-            
-        X[i] = X_entry
-     
-        
-        # y    
-        #TODO: requried index returned in list - fix later    
-        y_index = labels.index[labels['Video_File_Name'] == data_file[:-4]].tolist() 
-        y[i] = float(labels['Mastery'][y_index[0]])
-        
-        i += 1
-        
-        
-    return X,y
+import helper
 
     
+#set variables manually
+feat_dir  = 'all_labels.csv'
+label_folder = 'train_files/'
+column_title = 'emotionAfter'
+valueOfInterest = 'Interest'
+k = 5
+datafiles = 110
+
+   
+def RF_Classifier(X,y, test_X, y_true):
+    from sklearn.ensemble import RandomForestClassifier 
     
-def RF_regressor(X,y, test_X, y_true):
-    from sklearn.ensemble import RandomForestRegressor
-    from sklearn.metrics import accuracy_score
-    
-    
-    print('fds')
-    regr = RandomForestRegressor(max_depth=2, random_state=0)    
+    regr = RandomForestClassifier(max_depth=2, random_state=0)    
     regr.fit(X, y)
     
-#    print(regr.feature_importances_)  
-
-#    pd.DataFrame(test_X)
-    
+   
     y_pred = np.zeros(len(y_true))
-    for i in range(0,len(test_X[0])):
-        print(regr.predict(test_X[i]))
-        y_pred[i] = regr.predict(test_X[i])    
     
-    return accuracy_score(y_true, y_pred)    
+    y_pred = regr.predict(test_X)    
+    
+    return y_pred    
+
+    
+def train(X, y, k, kFoldIndeces):
+    resultTable = {}
+    
+    X_train = []
+    for trial in range(k):
+        X_train = X[kFoldIndeces[trial][0]]
+        X_test = X[kFoldIndeces[trial][1]]
+        
+        y_train = y[kFoldIndeces[trial][0]]
+        y_test = y[kFoldIndeces[trial][1]]
+        print(y_test)
+    
+        y_pred =  RF_Classifier(X_train,y_train, X_test, y_test)
+        resultTable[trial] = [y_test, y_pred]
+    
+    return resultTable
+
+def getMetrics(resultTable,k,datafiles):
+    from sklearn.metrics import confusion_matrix    
+    
+    scores = []
+    confusionMatrices = []
+    for i in range(0,k):
+        score = helper.accuracyScore(resultTable[i][0], resultTable[i][1])
+        confusionMatrix = confusion_matrix(resultTable[i][0], resultTable[i][1])
+        confusionMatrices.append(confusionMatrix)
+        scores.append(score)
+    
+    meanScore = round(float(sum(scores)/len(scores)),3) * 100 # accuracy in percentage
+    
+#    norm_conf_matrix = np.around((sum(confusionMatrices)/datafiles),decimals =1)
+    norm_conf_matrix = sum(confusionMatrices)/datafiles
+        
+    return meanScore, norm_conf_matrix
     
     
 def main():
+       
      #train
-     X,y = features('eureka_train_labels.csv', 'eureka_train/')
-     
-     #test
-     test_X, y_true = features('eureka_test_labels.csv', 'eureka_test/')
-     
-     score = RF_regressor(X,y,test_X,y_true)
-
-#     score = RF_regressor(pd.DataFrame(X),pd.DataFrame(y), \
-#                                               pd.DataFrame(test_X), pd.DataFrame(y_true))
-     
-     print(score)
+    X,y = helper.features(feat_dir, label_folder, column_title)
+    y = y.values.flatten().tolist()
+    X,y = helper.shuffle(X,y)
     
+    new_y = [0]*len(y)
+
+    new_y = helper.binarize(y,valueOfInterest)
+    y = new_y
+    
+    kFoldIndeces = helper.TrainTestSplit(X,y,k)
+     
+    resultTable = train(X, np.array(y), k, kFoldIndeces)
+    meanScore, norm_conf_matrix = getMetrics(resultTable,k,datafiles)
+    
+    print("accuracy = ", meanScore)
+
+    return X, y, resultTable, meanScore, norm_conf_matrix
        
 if __name__ == '__main__':
-    main()    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-    
+    X, y, resultTable,meanScore, norm_conf_matrix = main()    
 
 
 
